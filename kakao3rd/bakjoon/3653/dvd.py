a7584d896fd8bbedd495d57ba1893d12a8b67e5f
ecycle <lsh0706@echomarketing.co.kr>
2018-01-17 19:33:43 +0900

    == 3653 py 추가 ==

---
+++ kakao3rd/bakjoon/3653/dvd.py
@@ -0,0 +1,22 @@
+import sys
+
+def solution (nm , mvOrder):
+
+    n = max(mvOrder)
+    mList = list(range(1,n+1))
+
+
+
+
+    return 0;
+
+
+
+
+
+times = int(input("times : "))
+nm = input("nm : ")
+mvOrder = input("Movie order : ")
+
+for i in range (times):
+    solution(map(int, nm.split()),map(int, mvOrder.split()))
