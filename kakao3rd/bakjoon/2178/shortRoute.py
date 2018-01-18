0d9d6823527ff27e3ceb8c37d61222d814f984a7
ecycle <lsh0706@echomarketing.co.kr>
2018-01-18 18:51:04 +0900

    == 2178 수정 ==

--- kakao3rd/bakjoon/2178/shortRoute.py
+++ kakao3rd/bakjoon/2178/shortRoute.py
@@ -3,64 +3,54 @@
 
 
 cnt = 0
-x=[0 for x in range(100)]
-y=[0 for y in range(100)]
-ll=[0 for l in range(100)]
+x=list(range(100))
+y=list(range(100))
+ll=list(range(100))
 
-def start(nn, mm, qq):
+def start(xpos, ypos, count):
     global cnt
     global x
     global y
     global ll
-    x[cnt] = nn
-    y[cnt] = mm
-    ll[cnt] = qq
-    cnt += 1
+    x[cnt] = xpos
+    y[cnt] = ypos
+    ll[cnt] = count
+    cnt = cnt + 1
 
-def dfs(n, m ,orimap):
-   pos = 0
-   xx = 0
-   yy = 0
-   map = [[0 for col in range(m)] for row in range(n)]
-   for i in orimap:
+def solution(n, m ,orires):
+   idx = 0
+   xIndex = 0
+   yIndex = 0
+   res = [[0 for col in range(m)] for row in range(n)]
+   for i in orires:
        for j in str(i):
-           map[xx][yy] = int(j)
-           yy += 1
-       xx += 1
-       yy = 0
-
-   print("map", map)
+           res[xIndex][yIndex] = int(j)
+           yIndex += 1
+       xIndex += 1
+       yIndex = 0
+   print("res", res)
    start(0, 0, 1)
+   while (idx < cnt and ( x[idx] != m-1  or y[idx] != n-1 )):
+       res[y[idx]][x[idx]] = 0
+       if y[idx] > 0 and res[y[idx] -1 ][x[idx]] == 1:
+           start(x[idx], y[idx] -1, ll[idx]+1)
+       if y[idx] < n-1 and res[y[idx] + 1][x[idx]] == 1:
+           start(x[idx], y[idx] + 1, ll[idx] + 1)
+       if x[idx] > 0 and res[y[idx]][x[idx]-1] == 1:
+           start(x[idx] -1, y[idx], ll[idx] + 1)
+       if x[idx] < m-1 and res[y[idx]][x[idx]+1] == 1:
+           start(x[idx] +1 , y[idx], ll[idx] + 1)
 
-   while (pos < cnt and ( x[pos] != m-1  or y[pos] != n-1 )):
-       map[y[pos]][x[pos]] = 0
+       idx += 1
+       print("res",res)
 
-       if y[pos] > 0 and map[y[pos] -1 ][x[pos]] == 1:
-           start(x[pos], y[pos] -1, ll[pos]+1)
-           print("up")
-
-       if y[pos] < n-1 and map[y[pos] + 1][x[pos]] == 1:
-           start(x[pos], y[pos] + 1, ll[pos] + 1)
-           print("down")
-
-       if x[pos] > 0 and map[y[pos]][x[pos]-1] == 1:
-           start(x[pos] -1, y[pos], ll[pos] + 1)
-           print("left")
-
-       if x[pos] < m-1 and map[y[pos]][x[pos]+1] == 1:
-           start(x[pos] +1 , y[pos], ll[pos] + 1)
-           print("right")
-
-       pos += 1
-       print("map",map)
-
-   if pos < cnt :
+   if idx < cnt :
        print(x)
        print(y)
        print(ll)
-       print(ll[pos])
+       print(ll[idx])
 
 
 
-orimap = [101111,101010,101011,111011]
-dfs(4,6, orimap)
+map = [101111,101010,101011,111011]
+solution(4,6, orires)
